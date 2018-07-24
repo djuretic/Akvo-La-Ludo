1bb29601b68d28187db24111bb471ed055438447
EthanHans3n <1ethanhansen@gmail.com>
2018-07-23 21:07:15 -0700

    wrong file

--- RMS-Inventory.py
+++
@@ -1,6 +0,0 @@
-#load text socument with inventory into the program here
-entered = str(input("Options: add, change, delete ")).lower()
-while (entered != "exit") :
-    if (entered == "add") :
-        #get more responses to add to the dictionary
-    entered = str(input("Options: add, change, delete ")).lower()
